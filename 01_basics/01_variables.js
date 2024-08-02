527c00dd890494e078b5b452dff469d7952bfc07
Parth1426 <parthdeshmukh1405@gmail.com>
2024-08-02 07:42:47 +0000

    first change

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144553
+let accountEmail ="parth@.com"
+var accountPassword = "12345"
+accountCity= "pune"
+let accountState;
+
+//accoundId = 2 // not allowed
+
+accountEmail ="sharyu@.com"
+accountPassword = "5647478"
+accountCity= "Delhi"
+
+/*
+prefer not to use var keyword
+because of issue in block scope and functional scope
+
+*/
+
+console.table([accountId, accountEmail , accountPassword , accountCity,accountState])
+
+

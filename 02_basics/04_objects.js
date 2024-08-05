16105d8be32431718c4cd9698d73f0d41f482126
Parth1426 <parthdeshmukh1405@gmail.com>
2024-08-05 09:47:13 +0000

    objects nd api nd json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -42,12 +42,44 @@ const obj3 = {...obj1, ...obj2}
 // }
 // user[1].email
 
-console.log(tinderuser);//{ id: '123abc', name: 'sammy', isloggedin: false }
-console.log(Object.keys(tinderuser));//[ 'id', 'name', 'isloggedin' ]
-console.log(Object.values(tinderuser));//[ '123abc', 'sammy', false ]
-console.log(Object.entries(tinderuser));//[ [ 'id', '123abc' ], [ 'name', 'sammy' ], [ 'isloggedin', false ] ]
+// console.log(tinderuser);//{ id: '123abc', name: 'sammy', isloggedin: false }
+// console.log(Object.keys(tinderuser));//[ 'id', 'name', 'isloggedin' ]
+// console.log(Object.values(tinderuser));//[ '123abc', 'sammy', false ]
+// console.log(Object.entries(tinderuser));//[ [ 'id', '123abc' ], [ 'name', 'sammy' ], [ 'isloggedin', false ] ]
 
-console.log(tinderuser.hasOwnProperty('isloggedin'));//true
+// console.log(tinderuser.hasOwnProperty('isloggedin'));//true
+
+//***************************************destructuring************************************** 
+
+const course ={
+    coursename : "js in hindi",
+    price : "999",
+    courseinstructor : "hitesh"
+}
+
+//course.courseinstructor
+
+//const {courseinstructor} = course
+const {courseinstructor : instruct} = course
+//console.log(courseinstructor)//hitesh
+console.log(instruct)//hitesh
+
+//****************************************API*************************************** 
+//JSON is the structure in which the values come to users
+// {
+//     "name":"parth",
+//    " coursename" : "js in hindi"   //api can be written as objects
+//     "price" : "free"
+// }
+
+//{
+    //this is JSON format
+//}
+
+//randomuserme most used api
+//also api can be given in array
+
+
 
 
 

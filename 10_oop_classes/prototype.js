e132ea6b01b3918796f3ceab1dfab7d00f2c844c
Parth1426 <parthdeshmukh1405@gmail.com>
2024-08-17 04:41:52 +0000

    ...

--- 10_oop_classes/prototype.js
+++ 10_oop_classes/prototype.js
@@ -10,20 +10,31 @@ let heropower ={
     spiderman: "sling",
 
     getSpiderPower: function(){
-        console.log(`spidy power is ${this.spiderman}`);
+       // console.log(`spidy power is ${this.spiderman}`);
     }
 }
 
 Object.prototype.parth=function(){
-    console.log(`parth is present in all objects`);
+    //console.log(`parth is present in all objects`);
 
 }
 
 //heropower.parth()
 Array.prototype.heyparth=function(){
-    console.log(`parth says hello`);
+    //console.log(`parth says hello`);
 }
 
 myheros.parth()
 myheros.heyparth()
 //heropower.heyparth()//heropower.heyparth is not a function
+
+let anotherusername ="chaiaurcode     "
+
+String.prototype.truelength=function(){
+    console.log(`${this}`);
+    console.log(`true length is : ${this.trim().length}`)
+}
+
+
+anotherusername.truelength()
+"parth".truelength()
